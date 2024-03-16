b2408bfc868b9b5184d5462bdd892f926fb00245
AL Manar <AL Manar@DESKTOP-RGKRTGJ>
2024-03-16 15:12:11 +0300

    THE First Project

---
+++ main.py
@@ -0,0 +1,18 @@
+
+file = open("zh.txt" , "r")
+f = file.read()
+
+b = ['A','B','C','D','E','F','J','G','H','I','K','L','N','M','O','P','Q','R','S','T','Y','V','W','X','U','Z']
+n = []
+n= f
+k=0
+p=1
+cout = 0
+for i in f:
+    cout = 1+cout
+k=0
+for k in cout:
+    print("z")
+
+
+
