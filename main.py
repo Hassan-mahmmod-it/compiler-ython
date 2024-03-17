18081419f23c72017da6321b0eea1bd9b4aade88
AL Manar <AL Manar@DESKTOP-RGKRTGJ>
2024-03-17 19:53:44 +0300

    the first project

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-
-file = open("zh.txt" , "r")
-f = file.read()
-
-b = ['A','B','C','D','E','F','J','G','H','I','K','L','N','M','O','P','Q','R','S','T','Y','V','W','X','U','Z']
-n = []
-n= f
-k=0
-p=1
-cout = 0
-for i in f:
-    cout = 1+cout
-k=0
-for k in cout:
-    print("z")
+i=0
+for i in range(20):
+    if i%2==0:
+        print("the value is even :  " + str(i))
 
 
 
